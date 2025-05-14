0cd2a4f537186bf142e18033cc0809676bbf6aaa
onyx <onyxikongho9@gmail.com>
2025-05-14 15:43:12 +0100

    resolution exercice6

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,14 +1,13 @@
-let age;
-let nom;
-let estConnecte;
-let valeurNulle;
-let inconnue;
+let age=24;
+let nom="onyx";
+let estConnecte=true;
+let valeurNulle=null;
+let inconnue=undefined;
+console.log(age);
+console.log(nom);
+console.log(estConnecte);
+console.log(valeurNulle);
+console.log(inconnue);
 
-// Les lignes ne doivent pas être modifiées
-module.exports = {
-  age,
-  nom,
-  estConnecte,
-  valeurNulle,
-  inconnue,
-};
+
+
