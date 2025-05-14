fc5a0477fffe5080feb67f7fe696589d2fcb7e39
onyx <onyxikongho9@gmail.com>
2025-05-14 15:32:53 +0100

    resolution exercice5

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,13 +1,3 @@
-// ecrire le code de conversion ici
-let celsius;
-
-// Conversion Celsius à Fahrenheit
-function celsiusToFahrenheit(celsius) {
-  // ecrire le code de conversion ici
-  let fahrenheit;
-  // la ligne suivante ne doit pas être modifiée
-  return fahrenheit;
-}
-
-// les lignes suivantes ne sont pas à modifier
-module.exports = { celsiusToFahrenheit };
+let celsius=30
+let Fahrenheit=(celsius*9/5)+32
+console.log(Fahrenheit)
