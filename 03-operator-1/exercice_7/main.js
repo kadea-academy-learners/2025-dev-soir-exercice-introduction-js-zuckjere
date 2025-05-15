53355a63d3e368db8c776ce57efea6f397ef6643
joscal001 <amundalajoscal@gmail.com>
2025-05-15 01:28:03 +0100

    Resolution operator

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,5 +1,9 @@
-let ageEnfant, agePere, ageMere, ageGrandPere, ageOncle;
 // Ecrivez votre code ici
+let ageEnfant=20; 
+let agePere=40;
+let ageMere=agePere-5;
+let ageGrandPere=(ageMere*2)-(ageEnfant/2); 
+let ageOncle=ageGrandPere+10;
 
 // Ne pas modifier le code ci-dessous
 module.exports = {
--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,12 +1,22 @@
-let salaireMensuel,
-  loyer,
-  nourriture,
-  transport,
-  autresDepenses,
-  totalDepenses,
-  reste;
+let salaireMensuel=500;
+let loyer=salaireMensuel*0.3;
+let nourriture=salaireMensuel*0.2;
+let transport=salaireMensuel*0.1;
+let autresDepenses=50;
+autresDepenses+=25;
+let totalDepenses=(loyer+nourriture+transport+autresDepenses);
+let reste=salaireMensuel-totalDepenses;
 // Ecrivez votre code ici
+if (reste>=100) {
+  console.log("Budget bien géré.");
+} else {
+  console.log("Attention, budget serré");
+}
 
+let loisirs=salaireMensuel*1.5;
+  totalDepenses+=loisirs;
+  reste=salaireMensuel-totalDepenses;
+  
 // Ne pas modifier le code ci-dessous
 module.exports = {
   salaireMensuel,
