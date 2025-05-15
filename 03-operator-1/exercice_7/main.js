f97a69fd0f28101ce818c689e8b6de7ba1e00cf2
joscal001 <amundalajoscal@gmail.com>
2025-05-15 18:02:22 +0100

    Correction ageOncle

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -3,7 +3,7 @@ let ageEnfant=20;
 let agePere=40;
 let ageMere=agePere-5;
 let ageGrandPere=(ageMere*2)-(ageEnfant/2); 
-let ageOncle=ageGrandPere+10;
+let ageOncle=agePere+10;
 
 // Ne pas modifier le code ci-dessous
 module.exports = {
