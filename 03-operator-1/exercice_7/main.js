af1f5386c3a1d1030d026faa30051e71e20b9568
etsa kambandji <kmjetsa@gmail.com>
2025-05-15 12:41:33 +0200

    operator exercices on 80%

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,5 +1,15 @@
 let ageEnfant, agePere, ageMere, ageGrandPere, ageOncle;
 // Ecrivez votre code ici
+let ageenfant = 20;
+let agepere = ageenfant*2;
+console.log(agepere);
+let agemere = agepere-5;
+console.log(agemere);
+let agegrandpere = (agemere*2)+(ageenfant/2);
+console.log(agegrandpere);
+let ageoncle = agepere+10;
+console.log(ageoncle);
+
 
 // Ne pas modifier le code ci-dessous
 module.exports = {
--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,10 +1,11 @@
-let salaireMensuel,
-  loyer,
-  nourriture,
-  transport,
-  autresDepenses,
-  totalDepenses,
-  reste;
+let salairemensuel=500,
+  loyer= (500 * 30) / 100,
+  nourriture= (500 * 20) / 100,
+  transport= (500 * 10) / 100,
+  loisir= (500 * 15) / 100,
+  autresdepenses= 50+25,
+  totaldepenses= loyer + nourriture + transport + autresdepenses+ loisir,
+  reste= salairemensuel - totaldepenses;
 // Ecrivez votre code ici
 
 // Ne pas modifier le code ci-dessous
