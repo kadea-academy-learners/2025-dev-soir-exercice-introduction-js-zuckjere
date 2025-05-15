3cbe94e83ab173918c1eeccd14971aecd2580014
jeremyMvula <zuckjeremy0@gmail.com>
2025-05-15 17:37:43 +0100

    resolution exercice6

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,8 +1,11 @@
-let age;
-let nom;
-let estConnecte;
-let valeurNulle;
-let inconnue;
+let age = 4;
+let nom = "jeremy";
+let estConnecte = true;
+let valeurNulle = null;
+let inconnue = undefined;
+console.log(
+  age + " " + nom + " " + estConnecte + " " + valeurNulle + " " + inconnue
+);
 
 // Les lignes ne doivent pas être modifiées
 module.exports = {
