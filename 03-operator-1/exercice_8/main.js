9e69fa3bed5124d5402291d2f1bb17d011e2584c
etsa kambandji <kmjetsa@gmail.com>
2025-05-15 13:13:01 +0200

    operator exercices on 85%

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -2,8 +2,10 @@ let salairemensuel=500,
   loyer= (500 * 30) / 100,
   nourriture= (500 * 20) / 100,
   transport= (500 * 10) / 100,
-  loisir= (500 * 15) / 100,
   autresdepenses= 50+25,
+  totaldepenses= loyer + nourriture + transport + autresdepenses,
+  reste= salairemensuel - totaldepenses;
+  loisir= (500 * 15) / 100,
   totaldepenses= loyer + nourriture + transport + autresdepenses+ loisir,
   reste= salairemensuel - totaldepenses;
 // Ecrivez votre code ici
