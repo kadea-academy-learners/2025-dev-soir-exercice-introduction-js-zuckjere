67ba05cc3bb8b9ec93574c86c7981538455cb8d6
jeremyMvula <zuckjeremy0@gmail.com>
2025-05-15 18:03:40 +0100

    resolution exercice7

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,6 +1,14 @@
-let ageEnfant, agePere, ageMere, ageGrandPere, ageOncle;
 // Ecrivez votre code ici
-
+let ageEnfant = 14;
+let agePere = ageEnfant * 2;
+let ageMere = agePere - 5;
+let ageGrandPere = ageMere * 2 + ageEnfant / 2;
+let ageOncle = agePere + 10;
+console.log("L'âge est : " ageEnfant);
+console.log(agePere);
+console.log(ageMere);
+console.log(ageGrandPere);
+console.log(ageOncle);
 // Ne pas modifier le code ci-dessous
 module.exports = {
   ageEnfant,
