4956b31e13ff931f0f7c2841c024d05dce4af579
gael <manzodilagman92@gmail.com>
2025-05-19 18:23:24 +0100

    j'avais oubblie de push

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -6,7 +6,13 @@ let salaireMensuel,
   totalDepenses,
   reste;
 // Ecrivez votre code ici
-
+salaireMensuel=500;
+loyer=salaireMensuel*30/100;
+nourriture=20/100*salaireMensuel;
+transport=10/100*salaireMensuel;
+autresDepenses+=25autresDepenses;
+totalDepenses=loyer+nourriture+transport+autresDepenses;
+reste=salaireMensuel-totalDepenses;
 // Ne pas modifier le code ci-dessous
 module.exports = {
   salaireMensuel,
