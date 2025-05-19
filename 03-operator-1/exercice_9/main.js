c96be905439fafc1f2c6640538a93b6b60fd1194
etsa kambandji <kmjetsa@gmail.com>
2025-05-19 02:39:44 +0200

    i finish my exercice

--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -1,5 +1,17 @@
 let paul, marie, eric, clair, madameMukuna, joseph, sarah;
 // Ecrivez votre code ici
+let patrimoin= 60000000+40000000+20000000;
+let partenfant= patrimoin*75/100;
+let partfamille= patrimoin*25/100;
+paul= partenfant*(1/3);
+marie= partenfant*(1/3);
+alain= partenfant*(1/3);
+eric= alain*(1/2);
+clair= alain*(1/2);
+madameMukuna= partfamille*(1/3);
+joseph= partfamille*(1/3);
+sarah= partfamille*(1/3);
+console.log(paul,marie,eric,clair,madameMukuna,joseph,sarah);
 
 // Ne pas modifier le code ci-dessous
 module.exports = {
